cbfdf3d5c234283eb8bb482dd578127968cd3fe7
mgracesb <mercysbarcel@gmail.com>
2020-04-12 22:38:04 +0200

    add styles

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -3,9 +3,13 @@ import React from "react";
 const Preview = (props) => {
   return (
     <div>
-      <p>{props.name}</p>
-      <p>{props.job}</p>
-      <p>{props.description}</p>
+      <p>
+        {props.name}
+        <br />
+        {props.job}
+        <br />
+        {props.description}
+      </p>
     </div>
   );
 };
